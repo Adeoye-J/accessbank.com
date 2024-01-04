782c06a9f33cf5410ad6831db1aaf5b19f1712b6
Jeremiah Bankole <bankolejeremiahadeoye@gmail.com>
2024-01-04 10:38:31 +0100

    My First Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// // function forYou() {
+// const forYou = () => {
+//     let x = document.querySelector(".main-nav");
+//     if (x.style.display === "block") {
+//         x.style.display === "none";
+//     } else {
+//         x.style.display === "block";
+//     }
+// }
