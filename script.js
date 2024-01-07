168cae8086d6988bebba468823c465440ff13bc0
Jeremiah Bankole <bankolejeremiahadeoye@gmail.com>
2024-01-07 19:18:06 +0100

    This is my third commit

--- script.js
+++ script.js
@@ -29,3 +29,24 @@ rightNav.addEventListener("click", () => {
     imageWrapper.scrollBy({ left: imageWrapper.clientWidth, behavior: "smooth"});
 });
 
+const iconWrapper = document.getElementById("icon-wrapper");
+const prevIcon = document.getElementById("prev-icon");
+const nextIcon = document.getElementById("next-icon");
+
+prevIcon.addEventListener("click", () => {
+    iconWrapper.scrollBy({left: -iconWrapper.clientWidth, behavior: "smooth"});
+});
+nextIcon.addEventListener("click", () => {
+    iconWrapper.scrollBy({left: iconWrapper.clientWidth, behavior: "smooth"});
+});
+
+const testimonialWrapper = document.getElementById("testimonials");
+const prevTestimonial = document.getElementById("prev-testimonial");
+const nextTestimonial = document.getElementById("next-testimonial");
+
+prevTestimonial.addEventListener("click", () => {
+    testimonialWrapper.scrollBy({left: -testimonialWrapper.clientWidth, behavior: "smooth"});
+});
+nextTestimonial.addEventListener("click", () => {
+    testimonialWrapper.scrollBy({left: testimonialWrapper.clientWidth, behavior: "smooth"});
+});
